81b970f4d3e015b1e627e369c98e015f80696cbc
florianc <castelainflorian44@gmail.com>
2023-02-20 19:34:26 +0100

    feat(sql mermaid): start logic description

---
+++ sql-mermaid/main.asm
@@ -0,0 +1,41 @@
+
+section .data
+    read_bytes_size: equ 1
+    graph_header: db "graph LR", 10
+    graph_header_len: equ $-graph_header
+section .bss
+    read_input_buffer: resb read_bytes_size
+
+section .text
+    global _start
+    _start:                     ; read user input
+
+; WRITE "graph LR" to STDOUT
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, graph_header
+    mov edx, graph_header_len
+    int 80h
+; READ filename from STDIN
+
+
+; READ content of filename
+    ; clear current concated bytes
+    ; concat bytes until space is found
+    ; IF SPACE FOUND
+        ; IF grouped bytes = "CREATE"
+            ; set CREATE_FLAG to true
+            ; JMP READ content of filename
+    ; IF (grouped bytes = "TABLE" or "EXTERNAL") && CREATE_FLAG
+        ; JMP READ content of filename
+    ; ELSE
+        ; STORE the grouped bytes (it's the name of the table to create)
+        ; set CREATE_FLAG to false
+        ; JMP READ content of filename
+
+    mov eax,1
+    mov ebx,0
+    int 80H
+
+
+
